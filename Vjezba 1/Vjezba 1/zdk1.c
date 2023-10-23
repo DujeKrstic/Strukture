8b91606703ab749a668fa1f5e49cacbdea511c29
DujeKrstic <dkrsti00@fesb.hr>
2023-10-23 15:31:06 +0200

    Dovrsen prvi zadatak

--- Vjezba 1/Vjezba 1/zdk1.c
+++ Vjezba 1/Vjezba 1/zdk1.c
@@ -1,47 +1,120 @@
-﻿/*
-1. Napisati program koji prvo procita koliko redaka ima datoteka, tj. koliko ima studenata
-zapisanih u datoteci. Nakon toga potrebno je dinamicki alocirati prostor za niz struktura
-studenata (ime, prezime, bodovi) i ucitati iz datoteke sve zapise. Na ekran ispisati ime,
-prezime, apsolutni i relativni broj bodova.
-Napomena: Svaki redak datoteke sadrzi ime i prezime studenta, te broj bodova na kolokviju.
-relatvan_br_bodova = br_bodova/max_br_bodova*100
-*/
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define MAX_LINE 1024
+#define MAX_SIZE 50
+#define FILE_ERROR_OPEN -1
 
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#define MAX_SIZE (50)
-#define MAX_LINE (1024)
-#define FILE_ERROR_OPEN (-1)
+//Napisati program koji prvo pročita koliko redaka ima datoteka, tj.koliko ima studenata
+//zapisanih u datoteci.Nakon toga potrebno je dinamički alocirati prostor za niz struktura
+//studenata(ime, prezime, bodovi) i učitati iz datoteke sve zapise.Na ekran ispisati ime,
+//prezime, apsolutni i relativni broj bodova.
+//Napomena: Svaki redak datoteke sadrži ime i prezime studenta te broj bodova na kolokviju.
+//	relatvan_br_bodova = br_bodova / max_br_bodova * 100
 
-typedef struct _duje
-{
+typedef struct _student { //tip strukture je _student, a ime je Student
+
 	char name[MAX_SIZE];
 	char surname[MAX_SIZE];
 	double points;
 
-}Duje;
+}Student;
 
-int readNorowsInFile() {
+
+int readNoRowsInFile() {
+
 	int counter = 0;
-	FILE* FilePointer = NULL;
 	char buffer[MAX_LINE] = { 0 };
-	FilePointer = fopen("dat.txt", "r");
-	if (!FilePointer) {
-		printf("Greska.\n");
+
+	FILE* filePointer = NULL;
+	filePointer = fopen("students.txt", "r");
+
+	if (!filePointer) {
+		printf("\nNema nis");
 		return FILE_ERROR_OPEN;
 	}
-	while (!feof(FilePointer)) {
-		fgets(buffer, MAX_LINE, FilePointer);
+	while (!feof(filePointer)) {
+		fgets(buffer, MAX_LINE, filePointer); //cita liniju pa poveca counter(broj linija) sve dok ne dodje do kraja fajla
 		counter++;
-
 	}
-	fclose(FilePointer);
+	fclose(filePointer);
 	return counter;
 }
+
+//_________________ILI_________________________
+
+/* int readNoRowsInFile() {
+
+	int counter = 0;
+	char buffer[MAX_LINE] = {0};
+
+	FILE* filePointer=fopen("students.txt","r");
+
+	if (filePointer == NULL) {
+		printf("\nNije moguce otvorit datoteku");
+		return FILE_ERROR_OPEN;
+	}
+	if(filePointer != NULL){
+		while (!feof(filePointer)) {
+			fgets(buffer, MAX_LINE, filePointer); //cita liniju pa poveca counter(broj linija) sve dok ne dodje do kraja fajla
+			counter++;
+		}
+	}
+	fclose(filePointer);
+	return counter;
+}
+*/
+
+void readFromFile(Student* students, int noRows) {
+
+
+	FILE* filePointer = fopen("students.txt", "r");
+
+	if (filePointer == NULL) {
+		printf("\nNije moguce otvorit datoteku");
+		return FILE_ERROR_OPEN;
+	}
+
+	if (filePointer != NULL) {
+		for (int i = 0; i < noRows; i++) {
+			fscanf(filePointer, "%s %s %lf", students[i].name, students[i].surname, &students[i].points);
+		}
+	}
+	fclose(filePointer);
+
+}
+
+int RelativeNumberOfPoints(int max_number_of_points, double points) {
+	int rpoints;
+
+	rpoints = points / max_number_of_points * 100;
+
+	return rpoints;
+}
+
 int main() {
+
 	int noRows = 0;
-	noRows = readNorowsInFile();
-	printf("%d", noRows);
+	int i;
+	int max_number_of_points = 50;
+	double relative_points;
+	noRows = readNoRowsInFile();
+	printf("\nBroj redova je %d\n", noRows);
+
+	Student* students = (Student*)malloc(noRows * sizeof(Student)); //dinamicka alokacija memorije
+	readFromFile(students, noRows);
+
+	for (i = 0; i < noRows; i++) {
+
+		relative_points = RelativeNumberOfPoints(max_number_of_points, students[i].points);
+		printf("Ime: %s, Prezime: %s, Apsolutni broj bodova: %lf, Relativan broj bodova %lf\n", students[i].name, students[i].surname, students[i].points, relative_points);
+	}
+
 	return 0;
 }
+
+
+
+
+
