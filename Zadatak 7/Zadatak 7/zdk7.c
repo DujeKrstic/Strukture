e34989da15363656407955d810d7418ae33cf6a8
DujeKrstic <dkrsti00@fesb.hr>
2023-12-06 20:28:34 +0100

    Zapocet sedmi.

---
+++ Zadatak 7/Zadatak 7/zdk7.c
@@ -0,0 +1,15 @@
+/*
+* Napisati program koji pomocu vezanih listi (stabala) predstavlja strukturu direktorija.
+Omoguciti unos novih direktorija i pod-direktorija, ispis sadrzaja direktorija i
+povratak u prethodni direktorij. Tocnije program treba preko menija simulirati
+koristenje DOS naredbi: 1- "md", 2 - "cd dir", 3 - "cd..", 4 - "dir" i 5 – izlaz.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+}
