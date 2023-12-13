27914e0606b7bc1e8606d11fef305eb36a281c4d
DujeKrstic <dkrsti00@fesb.hr>
2023-12-13 21:07:24 +0100

    Pocet osmi uz timestampove.

--- Zadatak 7/Zadatak 7/zdk7.c
+++ Zadatak 7/Zadatak 7/zdk7.c
@@ -1,4 +1,4 @@
-?//7.Napisati program koji pomocu vezanih listi(stabala) predstavlja strukturu direktorija.
+//7.Napisati program koji pomocu vezanih listi(stabala) predstavlja strukturu direktorija.
 //Omoguciti unos novih direktorija i pod - direktorija, ispis sadrzaja direktorija i
 //povratak u prethodni direktorij.Tocnije program treba preko menija simulirati
 //koristenje DOS naredbi : 1 - "md", 2 - "cd dir", 3 - "cd..", 4 - "dir" i 5 – izlaz.
